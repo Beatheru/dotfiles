c94597a31b7f5c5492e827e37df29bbbc29ad0b5
Andrew Chen <beathandrewc@gmail.com>
2026-04-18 18:32:08 -0400

    add plymouth

--- install/main/scripts/limine.sh
+++ install/main/scripts/limine.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [[ "$OS_ID" != "cachyos" ]]; then
-  # Install Limine
-  log INFO "Installing Limine"
-  paru -S --noconfirm --needed limine limine-dracut-support limine-snapper-sync snapper btrfs-assistant
-  log INFO "Copying limine-entry-tool.conf to /etc/default/limine"
-  sudo cp /etc/limine-entry-tool.conf /etc/default/limine
+  log INFO "Copying limine configs"
+  sudo cp $INSTALL_CONFIGS/limine/default.conf /etc/default/limine
   sudo cp $INSTALL_CONFIGS/limine/limine.conf /boot
+  log INFO "Adding kernel params"
+  KERNEL_PARAMS=$(cat /etc/kernel/cmdline)
+  sudo sed -i -E "s|^KERNEL_CMDLINE\[default\]\+=\"\"|KERNEL_CMDLINE[default]+=\"$KERNEL_PARAMS quiet splash\"|" /etc/default/limine
+
   log INFO "Running limine-install"
   sudo limine-install
   log INFO "Running limine-update"
@@ -30,7 +31,6 @@ if [[ "$OS_ID" != "cachyos" ]]; then
   ## Change max snapshots to 10
   sudo sed -i 's/^NUMBER_LIMIT="50"/NUMBER_LIMIT="10"/' /etc/snapper/configs/{root,home}
   sudo sed -i 's/^NUMBER_LIMIT_IMPORTANT="10"/NUMBER_LIMIT_IMPORTANT="10"/' /etc/snapper/configs/{root,home}
-  echo "MAX_SNAPSHOT_ENTRIES=10" | sudo tee -a /etc/default/limine
   ## Change max space usage to be 30%
   sudo sed -i 's/^SPACE_LIMIT="0.5"/SPACE_LIMIT="0.3"/' /etc/snapper/configs/{root,home}
   ## Enable UKI and fallback
