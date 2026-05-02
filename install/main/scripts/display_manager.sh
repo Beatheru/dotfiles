4e962a6df56af5c5348072ab39551149970a29bd
Andrew Chen <beathandrewc@gmail.com>
2026-05-02 15:18:52 -0400

    use sddm

--- install/main/scripts/display_manager.sh
+++ install/main/scripts/display_manager.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
 
-if [[ "$OS_ID" = "cachyos" ]]; then
-  sudo mkdir -p /etc/sddm.conf.d
-  cat <<EOF | sudo tee /etc/sddm.conf.d/autologin.conf
-[Autologin]
-User=$USER
-Session=niri
-EOF
-else 
-  # Install Ly display manager
-  log INFO "Installing Ly"
-  paru -S --noconfirm --needed ly
-  log INFO "Disabling getty service"
-  sudo systemctl disable getty@tty1.service
-  log INFO "Enabling ly service"
-  sudo systemctl enable ly@tty1.service
-
-  log INFO "Configuring Ly autologin"
-  cat <<EOF | sudo tee /etc/ly/config.ini
-auto_login_session = niri
-auto_login_user = $USER
-EOF
-fi
+sudo systemctl enable sddm.service
